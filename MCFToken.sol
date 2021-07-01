dd20a688abbfb712e75f58790d1ad9419706b413
sk8er123 <sk8er12379@gmail.com>
2021-07-01 17:17:20 +0900

    Update MCFToken.sol

--- MCFToken.sol
+++ MCFToken.sol
@@ -1,9 +1,8 @@
-// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import '@openzeppelin/contracts/access/Ownable.sol';
-import '@openzeppelin/contracts/utils/Context.sol';
-import '@openzeppelin/contracts/utils/math/SafeMath.sol';
+import './Ownable.sol';
+import './Context.sol';
+import './SafeMath.sol';
 import './IERC20.sol';
 
 contract McfToken is Context, IERC20, Ownable {
